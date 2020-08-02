d6ba0e889c55aa24a42dd128af66e5cb6c1fbab9
houyong-cao <3246568319@qq.com>
2020-08-02 16:17:48 +0800

    hello world

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Worldt");
+        System.out.println("Hello Worldqt");
+        System.out.println("Hello Worldert");
+    }
+}
